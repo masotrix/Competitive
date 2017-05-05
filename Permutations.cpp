122e8e7d4e6e9ed9f4ff349e65bfbc7bb6d30fe4
masotrix <matihaeussler@gmail.com>
2017-05-05 10:53:40 -0300

    Update Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -3,100 +3,80 @@ using namespace std;
 
 
 void insertionSort(vector<int>& v, int n){
-	
-	int i, j, tmp;
- 	for (i=1; i<n; i++){
+  
+  int i, j, tmp;
+    for (i=1; i<n; i++){
 
-    	j=i;
-     	tmp=v[i];
+      j=i;
+      tmp=v[i];
 
-     	while (j>0 && tmp>v[j-1]){
-           v[j]=v[j-1];
-           j--;
-     	}
-    	v[j] = tmp;
-	}
+      while (j>0 && tmp>v[j-1]){
+        v[j]=v[j-1];
+        j--;
+      }
+      v[j] = tmp;
+  }
 }
 
-
-void time(clock_t begin){
-    clock_t now = clock();
-	cout <<"Time: "<<(double(now-begin) / CLOCKS_PER_SEC) << endl;
-}
 
 
 bool inc(vector<int>& v){
     
-    for (int i=1; i<v.size(); i++){
-    
-        if (v[i] < v[i-1]){ 
-
-            for (int j=0; j<i; j++)
-
-                if (v[j] > v[i]){ // Increment here
-
-                    swap(v[j], v[i]);
-                    insertionSort(v, i);
-                    //sort(v.begin(), v.begin()+i, greater<int>());
-                    
-                    return true;
-                }
+  for (int i=1; i<v.size(); i++){
+    if (v[i] < v[i-1]){ 
+      for (int j=0; j<i; j++){
+        if (v[j] > v[i]){ 
+          swap(v[j], v[i]);
+          insertionSort(v, i);  
+          return true;
         }
+      }
     }
-
-    return false;
+  }
+  return false;
 }
 
 
 int main(){
 
-    int t, k, sum, n=10;
-    bool b;
-    cin >> t;
+  int t, k, sum, n=10;
+  bool b;
+  cin >> t;
 
-    while (t--){
+  while (t--){
 
-        b = false;
-        vector<int> v(n);
-        for (int i=0; i<n; i++)
-            scanf("%d", &v[i]);
+    b = false;
+    vector<int> v(n);
+    for (int i=0; i<n; i++)
+        scanf("%d", &v[i]);
 
-        vector<int> vi(n);
-        cin >> k;
+    vector<int> vi(n);
+    cin >> k;
 
-        for (int i=0; i<n; i++)
-            vi[i] = n-i-1;
+    for (int i=0; i<n; i++)
+        vi[i] = n-i-1;
 
-        
 
-        clock_t begin = clock();
-
-        do { // Main Cycle 
-        
-            sum=0;
-            for (int i=n-1; i>=0; i--)
-                sum+=vi[i]*v[n-i-1];
-            
-            if (sum <= k){
-
-                for (int i=n-1; i>=0; i--)
-                    printf("%d ", vi[i]);
-                puts("");
-        
-                b = true;
-              
-                time(begin);
-                break;
-            } 
-        
-        } while (inc(vi));
-
-        if(!b){
-            puts("-1");
-            time(begin);
-        }
+    do { // Main Cycle 
     
-    }
+      sum=0;
+      for (int i=n-1; i>=0; i--)
+          sum+=vi[i]*v[n-i-1];
+      
+      if (sum <= k){
 
-	return 0;
+        for (int i=n-1; i>=0; i--)
+          printf("%d ", vi[i]);
+        puts("");
+
+        b = true; 
+        break;
+      } 
+    
+    } while (inc(vi));
+
+    if(!b) puts("-1");
+  }
+
+  return 0;
 }

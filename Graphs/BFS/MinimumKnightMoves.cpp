2459b337404a0d8701ab50cd3d4b6110adf1f74c
masotrix <matihaeussler@gmail.com>
2017-06-01 00:09:36 -0400

    Update MinimumKnightMoves.cpp

--- Graphs/BFS/MinimumKnightMoves.cpp
+++ Graphs/BFS/MinimumKnightMoves.cpp
@@ -1,42 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define coord pair<int,int>
-#define node pair<coord,int>
+#define neigh vector<int>
+#define node pair<pair<coord,int>,neigh>
 #define mkc(_x,_y) make_pair(_x,_y)
-#define mkn(_x,_y,_i) mkc(mkc(_x,_y),_i)
-#define x(_n) _n.first.first
-#define y(_n) _n.first.second
-#define i(_n) _n.second
-#define c(_n) _n.first
+#define mkn(_x,_y,_i) mkc(mkc(mkc(_x,_y),_i),neigh())
+#define c(_n) _n.first.first
+#define x(_n) c(_n).first
+#define y(_n) c(_n).second
+#define i(_n) _n.first.second
+#define n(_n) _n.second
+#define addN(_n,_nv) n(_n).push_back(_nv)
 
-vector<node> neighs =
-  {mkn(2,1,0),mkn(1,2,0),mkn(-2,1,0),mkn(2,-1,0),
-   mkn(-1,2,0),mkn(1,-2,0),mkn(-1,-2,0),mkn(-2,-1,0)};
 
-bool valid(node p){
+bool valid(node p, int H, int W){
 
-  if (x(p)>=0 && y(p)>=0 && x(p)<8 && y(p)<8)
+  if (x(p)>=0 && y(p)>=0 && x(p)<H && y(p)<W)
     return true;
   return false;
 }
 
-int bfs(int i1,int j1,int i2,int j2){
+vector<node> sides =
+  {mkn(2,1,0),mkn(1,2,0),mkn(-2,1,0),mkn(2,-1,0),
+   mkn(-1,2,0),mkn(1,-2,0),mkn(-1,-2,0),mkn(-2,-1,0)};
 
-  deque<node> q; int mov=-1;
-  q.push_back(mkn(i1,j1,0));
-  node p, pf=mkn(i2,j2,0); 
+vector<node> makeGraph(int H, int W){
+  vector<node> nodes;
+  map<coord,int> coords;
+  for (int i=0; i<H; i++){
+    for (int j=0; j<W; j++){
+      coords[mkc(i,j)] = i*W+j;
+    }
+  }
+  for (int i=0; i<H; i++){
+    for (int j=0; j<W; j++){
+      node n = mkn(i,j,0);
+      for_each(sides.begin(), sides.end(), [&](node s){
+        node nv = mkn(i+x(s),j+y(s), 0);
+        if (valid(nv,H,W)) addN(n,coords[c(nv)]);   
+      });
+      nodes.push_back(n);
+    }
+  }
+  return nodes;
+}
+
+int bfs(vector<node>& nodes, node& pi, node& pf){
+
+  deque<node> q; q.push_back(pi);  
+  int mov=-1; node p;
 
   while (true){
     
-    p=q.front();
-    q.pop_front();
+    p=q.front(); q.pop_front();
 
-    for_each (neighs.begin(), neighs.end(), [&](node n){
-         node p2 = mkn(x(p)+x(n),y(p)+y(n),i(p)+1);
-         if (valid(p2)){
-            if (c(p2)==c(pf)) { mov=i(p2); return; }
-            else q.push_back(p2);
-         }
+    for_each (n(p).begin(), n(p).end(), [&](int i){
+         i(nodes[i])=i(p)+1; q.push_back(nodes[i]);
+         if (c(nodes[i])==c(pf)) { mov=i(nodes[i]); return; }
     });
 
     if (mov!=-1) return mov;
@@ -46,16 +66,21 @@ int bfs(int i1,int j1,int i2,int j2){
 
 int main(){
  
-  int t; scanf("%d", &t);
-  string ini, fin;
-  int i1,j1,i2,j2;
+  int t; string ini, fin;
+  int i1,j1,i2,j2; node pi, pf;
+  vector<node> nodes = makeGraph(8,8);
+  scanf("%d", &t);
 
   while (t--){
     cin >> ini >> fin;
     if (ini == fin) {puts("0"); continue;}
     i1=ini[0]-'a'; j1=ini[1]-'1';
     i2=fin[0]-'a'; j2=fin[1]-'1';
-    printf("%d\n", bfs(i1,j1,i2,j2));
+    coord c1=mkc(i1,j1), c2=mkc(i2,j2);
+    for_each(nodes.begin(), nodes.end(), [&](node n){
+      if (c(n)==c1){ pi=n; i(pi)=0; } if (c(n)==c2) pf=n;
+    });
+    printf("%d\n", bfs(nodes, pi, pf));
   }
 
   return 0;
